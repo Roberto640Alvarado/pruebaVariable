f1d83f6ca78365d25f2cb162add2c45ed967b820
Roberto640Alvarado <00176620@uca.edu.sv>
2023-06-26 17:28:22 -0600

    Add

--- src/main/java/com/example/demo/prueba.java
+++ src/main/java/com/example/demo/prueba.java
@@ -4,18 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
-public class PruebaVariableApplication {
-	
-	@Value("${DATASOURCE_URL}")
+public class prueba {
+
+    @Value("${DATASOURCE_URL}")
     private String secret;
 
     public static void main(String[] args) {
         SpringApplication.run(PruebaVariableApplication.class, args);
     }
 
+    @PostConstruct
     public void printSecret() {
         System.out.println(secret);
     }
-	
 }
+
+
